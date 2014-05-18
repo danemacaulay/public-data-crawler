0b64e7017a584d1397136a6ee49c1397533b97ef
Dane Macaulay <dane.macaulay@gmail.com>
2014-05-17 19:09:35 -0500

    updates

--- crawl.js
+++ crawl.js
@@ -1,8 +1,10 @@
 var utils = require('utils');
 var fs = require('fs');
-var parcelData = fs.read('parcels.json');
+var parcelData = fs.read('data.json');
 parcelData = JSON.parse(parcelData);
+
 utils.dump("Processing " + parcelData.length + " parcels");
+
 main(utils, parcelData, fs);
 
 function main(utils, parcelData, fs){
@@ -14,7 +16,6 @@ function main(utils, parcelData, fs){
     casper = require('casper').create({
       clientScripts:  [
         'bower_components/jquery/index.js',
-        'bower_components/table-to-json/src/jquery.tabletojson.js',
       ],
       pageSettings: {
         loadImages:  false,
@@ -25,16 +26,23 @@ function main(utils, parcelData, fs){
     });
 
     ctrl.url = 'http://www.placer.ca.gov/Departments/Assessor/Assessment%20Inquiry.aspx';
-    ctrl.json = [];
-    ctrl.plots = parcelData;
-    // ctrl.plots = ["093380008000","038330018000","112290015000","043015041000","468010047000","054200007000","111100026000","458030006000","043015042000","054031075000","093380012000"];
+    ctrl.json = parcelData;
     processData();
   }
 
   function processData(){
     casper.start(ctrl.url);
-    ctrl.plots.forEach(function(plot){
-      processPlot(plot);
+    casper.then(function() {
+      this.evaluate(function() {
+        window.location = document.querySelector('iframe').src;
+      });
+    });
+    ctrl.json.forEach(function(plot, index){
+      if("Assessor ID Number" in plot){
+        utils.dump('skipping already processed plot');
+      }else{
+        processPlot(plot, index);
+      }
     });
     casper.run(function() {
       var that = this;
@@ -43,16 +51,11 @@ function main(utils, parcelData, fs){
     });
   }
 
-  function processPlot(plot){
-    casper.then(function() {
-      this.evaluate(function() {
-        window.location = document.querySelector('iframe').src;
-      });
-    });
+  function processPlot(plot, index){
     casper.wait(1000);
     casper.then(function() {
       this.evaluate(function(plot) {
-        document.querySelector('input[name="idfeeparcel"]').value = plot;
+        document.querySelector('input[name="idfeeparcel"]').value = plot.id;
         SubmitForm();
       }, plot);
     });
@@ -64,35 +67,56 @@ function main(utils, parcelData, fs){
     });
     casper.wait(1000);
     casper.then(function() {
+      casper.page.injectJs('/bower_components/jquery/index.js');
       var tableData = this.evaluate(function() {
         var data = {};
         var owners = [];
         var key, value;
         var found = false;
-        $('table tr').each(function(){
-          if($(this).find('.title').text() == 'Ownership'){
-            found = true;
-          }else if($(this).find('.title').text() == 'Building Description(s)'){
-            found = false;
-          }else if(found){
-            owners.push($(this).text().replace(/\s+/g, ' '));
-          }else{
-            key = $(this).find('td').first().text();
-            value = $(this).find('td').filter('.c2').text();
-            data[key] = value;
-          }
-        });
-        data["Ownership"] = owners.join(', ');
+        var $this;
+
+        $table = $('table tr');
+        if($table.length > 0){
+          $table.each(function(){
+            $this = $(this);
+            if($this.find('.title').text() == 'Ownership'){
+              found = true;
+            }else if($this.find('.title').text() == 'Building Description(s)'){
+              found = false;
+            }else if(found){
+              owners.push($this.text().replace(/\s+/g, ' '));
+            }else{
+              key = $this.find('td').first().text();
+              value = $this.find('td').filter('.c2').text();
+              data[key] = value;
+            }
+          });
+          data.Ownership = owners.join(', ');
+        }
         return data;
       });
       utils.dump(tableData);
-      ctrl.json.push(tableData);
+      ctrl.json[index] = merge(tableData, ctrl.json[index]);
     });
-    casper.wait(1000);
+    casper.wait(10000);
     casper.then(function() {
+      fs.write('data.json', JSON.stringify(ctrl.json), 'w');
       this.evaluate(function() {
-        window.location = 'http://www.placer.ca.gov/Departments/Assessor/Assessment%20Inquiry.aspx';
+        window.location = document.querySelector('a').href;
       });
     });
   }
+}
+
+/**
+ * Overwrites obj1's values with obj2's and adds obj2's if non existent in obj1
+ * @param obj1
+ * @param obj2
+ * @returns obj3 a new object based on obj1 and obj2
+ */
+ function merge(obj1,obj2){
+    var obj3 = {};
+    for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
+    for (var attrname2 in obj2) { obj3[attrname2] = obj2[attrname2]; }
+    return obj3;
 }
